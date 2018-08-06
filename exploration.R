ac3e5af3474c217270e82188452aeb415f75aa0e
Alex F. Bokov <bokov@uthscsa.edu>
2018-08-06 09:26:29 -0500

    Stopping point, added TODO list to exploration.R, added *.rdata and *.html to .gitignore

--- exploration.R
+++ exploration.R
@@ -6,7 +6,7 @@
 #' 
 #+ echo=FALSE, inlcude=FALSE, message=FALSE
 # if running in test-mode, uncomment the line below
-options(gitstamp_prod=F);
+#options(gitstamp_prod=F);
 .junk<-capture.output(source('global.R',echo=F));
 .depends <- 'data.R';
 .depdata <- paste0(.depends,'.rdata');
@@ -27,14 +27,27 @@ with(dat2,table(race_cd,v005_rc,useNA = 'always')) %>% addmargins() %>% pander()
 with(dat2,table(v044_hspnc_or_ltn,v010_spnsh_hspnc,useNA = 'always')) %>% 
   addmargins() %>% pander();
 
-#' ### TODO Next
+#' ### Next steps
 #' 
-#' * Create time-since-first-diagnosis variable
-#' * Create 0/1/2 variable for death (several raw variables)
-#' * Create 0/1/2 variable for recurrence
-#' * Create 0/1/2 variable for surgery date
-#' * Create unified Hispanic indicator
-#' * Mappings for numcode variables
+#' * TODO: Create time-since-first-diagnosis variable
+#' * TODO: Create TTE variable for death (several raw variables)
+#' * TODO: Create TTE variable for recurrence
+#' * TODO: Create TTE variable for surgery date
+#' * TODO: Create censoring variable for surgery/death
+#' * TODO: Create censoring variable for recurrence/death
+#' * TODO: Create unified Hispanic indicator
+#' * TODO: Map cancer status variable
+#' * TODO: Create unified comorbidity variable for:
+#'     * Diabetes
+#'     * Others?
+#' * TODO: Mappings for other numcode variables
+#' * TODO: Follow up re additional patient linkages, more recent NAACCR data
+#' * TODO: Re-run query with additional variables:
+#'     * EMR codes for metastasis
+#'     * median household income, 2016 and 2013
+#'     * HbA1c
+#'     * Family history of diabetes and cancer
+#' * TODO: Clean up TNM variables, in consultation with domain expert (Peter?)
 #' 
 #' ### Audit trail
 walktrail()[,-5] %>% pander(split.tables=600);
