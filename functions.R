f1d1cffd5113d2e3d3605770f1840aaf3d34ddec
Alex F. Bokov <bokov@uthscsa.edu>
2018-08-30 11:12:19 -0500

    Weird, apparently previous version of functions.R somehow had the old version of 'v()'. Now the multi-retcol functionality and 'formals()' friendliness properly implemented.

--- functions.R
+++ functions.R
@@ -61,6 +61,102 @@ survAUC<-function(Surv.rsp,Surv.rsp.new,lp,lpnew,times,time,lp0=0
     try(do.call(xx,args2use[[xx]]))}));
 }
 
+#' A function to re-order and/or rename the levels of a factor or 
+#' vector with optional cleanup.
+#'
+#' @param xx            a vector... if not a factor will be converted to 
+#'                      one
+#' @param lookuptable   matrix-like objects where the first column will
+#'                      be what to rename FROM and the second, what to
+#'                      rename TO. If there is only one column or if it's
+#'                      not matrix-like, the levels will be set to these
+#'                      values in the order they occur in `lookuptable`.
+#'                      If there are duplicated values in the first column
+#'                      only the first one gets used, with a warning.
+#'                      Duplicate values in the second column are allowed 
+#'                      and are a feature.
+#' @param reorder       Whether or not to change the order of the factor
+#'                      levels to match those in `lookuptable`. True by
+#'                      default (logical). By default is `TRUE`, is set to
+#'                      `FALSE` will try to preserve the original order of
+#'                      the levels.
+#' @param unmatched     Scalar value. If equal to -1 and `reorder` is `TRUE` 
+#'                      then unmatched original levels are prepended to the 
+#'                      matched ones in their original order of occurence. If 
+#'                      equal to 1, then appended in their original order of 
+#'                      occurrence. If some other value, then they are all 
+#'                      binned in one level of that name. The (empty) new ones 
+#'                      always go to the end.
+#' @param droplevels    Drop unused levels from the output factor (logical)
+#' @param case          Option to normalize case (`'asis'`` leaves it as it was)
+#'                      before attempting to match to `lookuptable`. One value 
+#'                      only
+#' @param mapnato       If the original levels contain `NA`s, what they should 
+#'                      be instead. They stay `NA` by default.
+#' @param remove        Vector of strings to remove from all level names (can
+#'                      be regexps) before trying to match to `lookuptable`.
+#' @param otherfun      A user-specified function to make further changes to
+#'                      the original level names before trying to match and
+#'                      replace them. Should expect to get the output from
+#'                      `levels(xx)` as its only argument.
+#' @param spec_mapper   A constrained lookup table specification that includes
+#'                      a column named `varname` in addition to the two columns
+#'                      that will become `from` and `to`. This is for extra
+#'                      convenience in projects that use such a table. If you
+#'                      aren't working on a project that already follows this
+#'                      convention, you should ignore this parameter.
+#' @param var           What value should be in the `varname` column of 
+#'                      `spec_mapper`
+#' @param fromto        Which columns in the `spec_mapper` should become the 
+#'                      `from` and `to` columns, respectively. Vector of length
+#'                      2, is `c('code','label')` by default.
+factorclean <- function(xx,lookuptable,reorder=T,unmatched=1
+                        ,droplevels=F,case=c('asis','lower','upper')
+                        ,mapnato=NA,remove='"',otherfun=identity
+                        ,spec_mapper,var,fromto=c('code','label')){
+  if(!is.factor(xx)) xx <- factor(xx);
+  lvls <- levels(xx);
+  lvls <- switch (match.arg(case)
+                   ,asis=identity,lower=tolower,upper=toupper)(lvls) %>% 
+    submulti(cbind(remove,'')) %>% otherfun;
+  levels(xx) <- lvls;
+  # Check to see if spec_mapper available.
+  if(!missing(spec_mapper)&&!missing(var)){
+    lookuptable <- subset(spec_mapper,varname==var)[,fromto];
+  }
+  # The assumption is that if you're passing just a vector or something like
+  # it, then you want to leave the level names as-is and just want to change
+  # the ordering
+  if(is.null(ncol(lookuptable))) lookuptable <- cbind(lookuptable,lookuptable);
+  # can never be too sure what kind of things with rows and columns are getting 
+  # passed, so coerce this to a plain old vanilla data.frame
+  lookuptable <- data.frame(lookuptable[,1:2]) %>% setNames(c('from','to'));
+  if(length(unique(lookuptable[,1]))!=nrow(lookuptable)) {
+    lookuptable <- lookuptable[match(unique(lookuptable$from),lookuptable$from),];
+    warning("You have duplicate values in the first (or only) column of your 'lookuptable' argument. Only the first instances of each will be kept");
+  }
+  if(reorder){
+    extras <- data.frame(from=lvls,to=NA,stringsAsFactors = F) %>%
+      subset(!from %in% lookuptable$from);
+    lookupfinal <- if(unmatched==-1) rbind(extras,lookuptable) else {
+      rbind(lookuptable,extras)};
+  } else {
+    lookupfinal <- left_join(data.frame(from=lvls,stringsAsFactors = F)
+                             ,lookuptable,by='from') %>% 
+      rbind(subset(lookuptable,!from%in%lvls));
+  }
+  # if the 'unmatched' parameter has the special value of -1 or 1, leave the 
+  # original names for the unmatched levels. Otherwise assign them to the bin
+  # this parameter specifies
+  lookupfinal$to <- with(lookupfinal,if(unmatched %in% c(-1,1)){
+    ifelse(is.na(to),from,to)} else {ifelse(is.na(to),unmatched,to)});
+  # Warning: not yet tested on xx's that are already factors and have an NA
+  # level
+  lookupfinal$to <- with(lookupfinal,ifelse(is.na(from),mapnato,to));
+  out <- factor(xx,levels=lookupfinal$from);
+  levels(out) <- lookupfinal$to;
+  if(droplevels) droplevels(out) else out;
+}
 
 
 #' into the specified existing or new level. That level is listed last
@@ -102,7 +198,9 @@ starkable <- function(xx,firstrowisdata=T,row.names=F,taildrop=1
 #' @param xx A \code{vector} of type \code{character} (required)
 #' @param searchrep A \code{matrix} with two columns of type \code{character} (required). The left column is the pattern and the right, the replacement.
 #' @param method One of 'partial','full', or 'exact'. Controls whether to replace only the matching regexp, replace the entire value that contains a matching regexp, or replace the entire value if it's an exact match.
-submulti <- function(xx,searchrep,method=c('partial','full','exact')){
+submulti <- function(xx,searchrep
+                     ,method=c('partial','full','exact'
+                               ,'starts','ends','startsends')){
   # if no method is specified by the user, this makes it take the first value
   # if a method is only partially written out, this completes it, and if the
   # method doesn't match any of the valid possibilities this gives an informativ
@@ -120,7 +218,13 @@ submulti <- function(xx,searchrep,method=c('partial','full','exact')){
          ,exact = {for(ii in rr)
            oo[grepl(searchrep[ii,1],oo,fixed=T)]<-searchrep[ii,2]}
            #oo <- gsub(searchrep[ii,1],searchrep[ii,2],oo,fixed = T)}
-         );
+         ,starts = {for(ii in rr)
+           oo <- gsub(paste0('^',searchrep[ii,1]),searchrep[ii,2],oo)}
+         ,ends = {for(ii in rr)
+           oo <- gsub(paste0(searchrep[ii,1],'$'),searchrep[ii,2],oo)}
+         ,startsends = {for(ii in rr)
+           oo <- gsub(paste0('^',searchrep[ii,1],'$'),searchrep[ii,2],oo)}
+  );
   oo;
 }
 
@@ -183,10 +287,68 @@ checkrun <- function(obj,EXPR,env=as.environment(-1)){
 #' @param ... 
 tte <- function(time,expr,...){
   event <- min(which(expr));
-  if(is.infinite(event)) etime <- max(time) else {
-    etime <- event;}
-  out <- etime - time;
+  # The +1 needed so that a series of followups where an 
+  # event never happens is always negative while a series
+  # of followups when an event is observed has a 0 in it
+  if(is.infinite(event)) etime <- max(time)+1 else {
+    etime <- time[event];}
+  out <- time - etime;
 }
+
+
+#' Take two vectors of names `ii` and `jj`, perform an aggregating 
+#' function on each vector respectively and then perform a comparison function 
+#' `fcmp` on the two aggregation results.
+#' 
+#' If the optional `env` is provided, returns a result vector 
+#' evaluated in the environment of env, otherwise return an unevaluated call.
+#'
+#' @param ii    A vector of object or variable names (required)
+#' @param jj    A vector of object or variable names (required)
+#' @param env   An optional list-like object containing above-named
+#'              objects.
+#' @param fii   The name (character) of a function that can take all the objects
+#'              represented in `ii` as its argument-list... 'pmax' by default.
+#' @param fjj   The name (character) of a function that can take all the objects
+#'              represented in `jj` as its argument-list. Set to `fii` by
+#'              default.
+#' @param fcmp  The name (character) of a function that will take the outputs of
+#'              functions named in `fii` and `fjj` as its two arguments. By 
+#'              default it's '<'.
+#' @param optii A named list of optional arguments to pass to `fii`. By default
+#'              it only name-value pair is `na.rm=T`
+#' @param optjj A named list of optional arguments to pass to `fii`. By default
+#'              it's set to `optjj`
+#'
+#' @return A call generated using the above arguments, unless `env`  is
+#'         given in which case the result of evaluating that call in the context
+#'         of `env`
+#' @export
+#'
+#' @examples comp_iijj(v(c_natf,dat1,retcol=c('colname','varname'))
+#'                    ,v(c_kcdiag,dat1,retcol=c('colname','varname')),dat1);
+#' 
+comp_iijj <- function(ii,jj,env,fii='pmax',fjj=fii,fcmp='>'
+                      ,optii=list(na.rm=T),optjj=optii){
+  ii <- lapply(ii,as.name); jj <- lapply(jj,as.name);
+  callii <- do.call(call,c(fii,ii,optii),quote=T);
+  calljj <- do.call(call,c(fjj,jj,optjj),quote=T);
+  out <- do.call(call,c(fcmp,callii,calljj),quote=T);
+  if(!missing(env)) out <- eval(out,envir = env);
+  return(out);
+}
+
+#' Code the output of `tte()` such that before an event observations get `0`
+#' during the (first) event observations get `1` and subsequent get `2`
+#' 
+#' By default it shifts the result by 1, producing a vector that can directly be
+#' used as the `event` in a `Surv(time,event)` type expression assuming you trim
+#' out the >1 values. To change the amount of shift, use the optional `shift`
+#' argument. To do something other than shifting by that amount, use the 
+#' optional `fn` variable which takes as its argument a function that can 
+#' operate on exactly two arguments.
+#' 
+cte <- function(...,shift=1,fn=`+`) fn(sign(pmax(...,na.rm=T)),shift);
 
 #' Delete all the junk in your environment, for testing
 clearenv <- function(env=.GlobalEnv) rm(list=setdiff(ls(all=T,envir=env),'clearenv'),envir=env);
@@ -245,6 +407,30 @@ autoboxplot <- function(pdata, xx, yy, zz, subset=T
 }
 
 getCall.data.frame <- getCall.gg <- function(xx) attr(xx,'call');
+
+#' From ... https://menugget.blogspot.com/2011/11/define-color-steps-for-colorramppalette.html#more
+#' Manually shape the intervals of a color gradient
+color.palette <- function(steps, n.steps.between=NULL, ...){
+  if(is.null(n.steps.between)) n.steps.between <- rep(0, (length(steps)-1))
+  if(length(n.steps.between) != length(steps)-1) stop("Must have one less n.steps.between value than steps")
+  
+  fill.steps <- cumsum(rep(1, length(steps))+c(0,n.steps.between))
+  RGB <- matrix(NA, nrow=3, ncol=fill.steps[length(fill.steps)])
+  RGB[,fill.steps] <- col2rgb(steps)
+  
+  for(i in which(n.steps.between>0)){
+    col.start=RGB[,fill.steps[i]]
+    col.end=RGB[,fill.steps[i+1]]
+    for(j in seq(3)){
+      vals <- seq(col.start[j], col.end[j], length.out=n.steps.between[i]+2)[2:(2+n.steps.between[i]-1)]  
+      RGB[j,(fill.steps[i]+1):(fill.steps[i+1]-1)] <- vals
+    }
+  }
+  
+  new.steps <- rgb(RGB[1,], RGB[2,], RGB[3,], maxColorValue = 255)
+  pal <- colorRampPalette(new.steps, ...)
+  return(pal)
+}
 
 
 #' From ... http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
@@ -494,31 +680,38 @@ stratatable <- function(xx,vars=NULL,...){
 #'                   (currently) enforced programmatically.
 v <- function(var,dat
               ,matchcol='colname'
+              # todo: let retcol take a vector argument
               ,retcol=matchcol
-              ,dictionary=dct0) {
+              ,dictionary=dct0
+              ,asname=F) {
   # convenience function: if forgot what column names are available, call with
   # no arguments and they will be listed
   if(missing(var)) return(names(dictionary));
   # support both standard or non-standard evaluation
   var<-as.character(substitute(var));
-  # if a 'dat' argument is given, restrict the output so that only results having
-  # having values found in the colnames of 'dat' are returned. The 'formals' clause
-  # of the 'if' statement is to let the user set local defaults for this function
-  if(!missing(dat)||!identical(as.character(formals(sys.function())$dat),'')){
-    dictionary <- dictionary[dictionary[[matchcol]]%in%colnames(dat),];}
   # TODO: Think about what to do when nothing matches... not necessarily an error
   #       condition, might just be something to warn about and move on.
-  out<-dictionary[dictionary[[var]],retcol][[1]];
+  out <- unique(as.vector(na.omit(unlist(dictionary[dictionary[[var]],retcol]))));
+  # Why did I do the above in such a complicated way below? 
+  #out<-dictionary[dictionary[[var]],retcol][[1]];
   # if something other than matchcol is returned, give it a name to make it 
   # easier to align with column names in the data
-  if(!identical(retcol,matchcol)){
-    browser();
-    out<-setNames(out,dictionary[dictionary[[var]],matchcol][[1]]);
-  }
-  # 'na.omit()' needed because we allows the 'dictionary' object to have NAs instead
+  #if(retcol != matchcol){
+  #  out<-setNames(out,dictionary[dictionary[[var]],matchcol][[1]]);
+  #}
+  
+  # 'na.omit()' needed because we allow the 'dictionary' object to have NAs instead
   # of FALSEs. 'c()' needed to strip na.omit metadata, so the output is a plain
   # old vector
-  return(c(na.omit(out)));
+  #out <- c(na.omit(out));
+  # if a 'dat' argument is given, restrict the output so that only results having
+  # having values found in the colnames of 'dat' are returned.
+  #if(!missing(dat)) out <- out[out%in%colnames(dat)];
+  if(!is(try(cnames<-colnames(dat),silent = T),'try-error')) {
+    out <- out[out%in%cnames];}
+  if(asname) out <- lapply(out,as.name);
+  #return(unname(out));
+  return(out);
   }
 
 #'.This function will create a variable summary table that will provide 
