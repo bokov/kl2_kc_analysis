371ff2e3875aff45af29e600f49c04db633b08b5
Alex F. Bokov <bokov@uthscsa.edu>
2018-09-14 18:04:45 -0500

    Updates to mk_progress_report.sh so that it can compile any file and template to docx

--- mk_progress_report.sh
+++ mk_progress_report.sh
@@ -1,1 +1,9 @@
-pandoc -s -S --reference-docx nt_styletemplate.docx exploration.docx -o progress_report.docx
+#! /bin/bash
+input=${1:-"exploration.docx"};
+output=${2:-"NONE"};
+styletemplate=${3:-"nt_styletemplate.docx"};
+if [ "$output" == "NONE" ];
+    then output=$(echo $input|sed s/exploration/progress/g|sed s/.docx/_report.docx);
+    fi;
+pandoc -s -S --reference-docx $styletemplate $input -o $output;
+
