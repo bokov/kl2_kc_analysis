b21da402f34d675b4f01e8b01130ead63672c19c
Alex F. Bokov <bokov@uthscsa.edu>
2018-09-14 20:28:45 -0500

    Bugfix to mk_progress_report.sh

--- mk_progress_report.sh
+++ mk_progress_report.sh
@@ -3,7 +3,7 @@ input=${1:-"exploration.docx"};
 output=${2:-"NONE"};
 styletemplate=${3:-"nt_styletemplate.docx"};
 if [ "$output" == "NONE" ];
-    then output=$(echo $input|sed s/exploration/progress/g|sed s/.docx/_report.docx);
+    then output=$(echo $input|sed s/exploration/progress/g|sed s/.docx/_report.docx/g);
     fi;
 pandoc -s -S --reference-docx $styletemplate $input -o $output;
 
