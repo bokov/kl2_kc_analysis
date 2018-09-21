db33f6fe29805f80d63a717dd2ea364cad64f216
Alex F. Bokov <bokov@uthscsa.edu>
2018-09-20 23:39:32 -0500

    mk_progress_report.sh;rmd;No longer renaming exploration.R to progress_report.XXX.

--- mk_progress_report.sh
+++ mk_progress_report.sh
@@ -3,8 +3,8 @@ input=${1:-"exploration.R"};
 output=${2:-"NONE"};
 styletemplate=${3:-"nt_styletemplate.docx"};
 if [ "$output" == "NONE" ];
-    then output=$(echo $input|sed s/exploration/progress/g|sed "s/\.R$\|\.Rmd$/.docx/g");
+    then output=$(echo $input|sed "s/\.R$\|\.Rmd$//g");
     fi;
 #pandoc -s -S --reference-docx $styletemplate $input -o $output;
-R -e "library(rmarkdown);render('$input',output_file = '$output', output_format = word_document(reference_docx = '$styletemplate',keep_md=T));render('$input',output_file='$output',output_format=html_document(keep_md=T));";
+R -e "library(rmarkdown);render('$input',output_file = '$output.docx', output_format = word_document(reference_docx = '$styletemplate',keep_md=T));render('$input',output_file='$output.html',output_format=html_document(keep_md=T));";
 
