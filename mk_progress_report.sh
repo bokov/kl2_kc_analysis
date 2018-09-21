a36d8d9a674662baa5cc10db575ce750350c978e
Alex F. Bokov <bokov@uthscsa.edu>
2018-09-21 16:27:23 -0500

    mk_progress_report.sh;rmd;Updated to use the pandoc-crossref filter

--- mk_progress_report.sh
+++ mk_progress_report.sh
@@ -6,5 +6,11 @@ if [ "$output" == "NONE" ];
     then output=$(echo $input|sed "s/\.R$\|\.Rmd$//g");
     fi;
 #pandoc -s -S --reference-docx $styletemplate $input -o $output;
-R -e "library(rmarkdown);render('$input',output_file = '$output.docx', output_format = word_document(reference_docx = '$styletemplate',keep_md=T));render('$input',output_file='$output.html',output_format=html_document(keep_md=T));";
+R -e "library(rmarkdown);\
+render('$input',output_file = '$output.docx'\
+  , output_format = word_document(reference_docx = '$styletemplate'\
+    ,keep_md=T,pandoc_args=c('--filter','pandoc-crossref')));\
+render('$input',output_file='$output.html'\
+  ,output_format=html_document(keep_md=T\
+    ,pandoc_args=c('--filter','pandoc-crossref')))";
 
