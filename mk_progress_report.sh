cb7369ea26695ea23b9042f8780dbc3f9ae3485c
Alex F. Bokov <bokov@uthscsa.edu>
2018-09-20 23:08:52 -0500

    mk_progress_report.sh;rmd;Updated script to generate .docx and .html (and the .rdata files on which they depend) via R instead of directly through pandoc.

--- mk_progress_report.sh
+++ mk_progress_report.sh
@@ -5,5 +5,6 @@ styletemplate=${3:-"nt_styletemplate.docx"};
 if [ "$output" == "NONE" ];
     then output=$(echo $input|sed s/exploration/progress/g|sed s/.docx/_report.docx/g);
     fi;
-pandoc -s -S --reference-docx $styletemplate $input -o $output;
+#pandoc -s -S --reference-docx $styletemplate $input -o $output;
+R -e "library(rmarkdown);render('$input',output_file = '$output', output_format = word_document(reference_docx = '$styletemplate',keep_md=T));render('$input',output_file='$output',output_format=html_document(keep_md=T));";
 
