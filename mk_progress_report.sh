0aadfba9e5dd54538bd2eedb354f8c2899be12dd
Alex F. Bokov <bokov@uthscsa.edu>
2018-09-20 23:24:27 -0500

    mk_progress_report.sh;rmd;Updated to use render

--- mk_progress_report.sh
+++ mk_progress_report.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-input=${1:-"exploration.docx"};
+input=${1:-"exploration.R"};
 output=${2:-"NONE"};
 styletemplate=${3:-"nt_styletemplate.docx"};
 if [ "$output" == "NONE" ];
-    then output=$(echo $input|sed s/exploration/progress/g|sed s/.docx/_report.docx/g);
+    then output=$(echo $input|sed s/exploration/progress/g|sed "s/\.R$\|\.Rmd$/.docx/g");
     fi;
 #pandoc -s -S --reference-docx $styletemplate $input -o $output;
 R -e "library(rmarkdown);render('$input',output_file = '$output', output_format = word_document(reference_docx = '$styletemplate',keep_md=T));render('$input',output_file='$output',output_format=html_document(keep_md=T));";
