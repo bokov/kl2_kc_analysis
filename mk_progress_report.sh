d9ca3d5bdb5ebb0ab389ce98022663f25de4865c
Alex F. Bokov <bokov@uthscsa.edu>
2018-10-10 16:10:35 -0500

    mk_progress_report.sh;utils;Now have bash-style flags to pass optional arguments, including -w which can currently be docx, html, or docx+html (or any other string contining both substrings, which will be parsed as both)

--- mk_progress_report.sh
+++ mk_progress_report.sh
@@ -1,16 +1,52 @@
 #! /bin/bash
-input=${1:-"exploration.R"};
-output=${2:-"NONE"};
-styletemplate=${3:-"nt_styletemplate.docx"};
+
+declare OPTARG;
+declare OPTIND;
+declare opt;
+declare input;
+declare output;
+declare styletemplate;
+declare which;
+
+input="exploration.R";
+output="NONE";
+styletemplate="nt_styletemplate.docx";
+which="html+docx";
+
+while getopts "i:o:s:w:" opt; do
+        echo "OPT=$opt";
+        case $opt in
+            i) input="$OPTARG" ;;
+            o) output="$OPTARG" ;;
+            s) styletemplate="$OPTARG" ;;
+            w) which=$OPTARG ;;
+            \?) echo "Invalid option: -$OPTARG" ;;
+        esac
+    done;
+
 if [ "$output" == "NONE" ];
     then output=$(echo $input|sed "s/\.R$\|\.Rmd$//g");
     fi;
 #pandoc -s -S --reference-docx $styletemplate $input -o $output;
-R -e "library(rmarkdown);\
-render('$input',output_file = '$output.docx'\
-  , output_format = word_document(reference_docx = '$styletemplate'\
-    ,keep_md=T,pandoc_args=c('--filter','pandoc-crossref')));\
-render('$input',output_file='$output.html'\
+
+rcommand="library(rmarkdown); ";
+
+if [[ "$which" == *docx* ]]; then
+rcommand="$rcommand render('$input',output_file = '$output.docx'\
+    , output_format = word_document(reference_docx = '$styletemplate'\
+        ,keep_md=T,pandoc_args=c('--filter','pandoc-crossref')));\
+";
+fi;
+
+if [[ "$which" == *html* ]]; then
+rcommand="$rcommand render('$input',output_file = '$output.html'\
   ,output_format=html_document(keep_md=T\
-    ,pandoc_args=c('--filter','pandoc-crossref')))";
+    ,pandoc_args=c('--filter','pandoc-crossref')));\
+";
+fi;
+
+echo "WHICH: $which";
+echo $rcommand;
+
+R -e "$rcommand";
 
