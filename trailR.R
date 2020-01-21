e37c6d1c10747522a918d965873504a65b0b74b3
Alex Bokov <bokov@uthscsa.edu>
2020-01-21 07:18:04 -0600

    trailR.R; tweak; On 'gitstamp()' , get the 'production' argument from the options first

--- trailR.R
+++ trailR.R
@@ -3,35 +3,35 @@
 #' author: "Wilson, Bokov"
 #' date: "03/21/2018"
 #' ---
-#' 
+#'
 #' This is going to get called via getOption('trail',tinit())
 #' The fields of trail are:
 #' time = timestamp
 #' type = type of information (file, rdata, info, gitstamp, seed, etc)
 #' name = the name of that specific item, should be unique
-#' value = Almost anything. Non atomic values should be assigned for example like this: 
+#' value = Almost anything. Non atomic values should be assigned for example like this:
 #'   `trail[nrow(trail),'value'] <- list(list(mtcars))`
 #'   For items of type 'file' the path will be stored here, for 'seed', the random seed, for 'gitstamp'
 #'   the current branch.
 #' hash = For 'file' and 'rdata' the md5 checksum of the file, for 'gitstamp' the commit hash
 #' trail = For 'rdata', if a trail object exists, dump it into this field. And it can have its own trail objects
 #'   so we have an arbitrarily long "family tree" of ancestor objects!
-#' 
+#'
 #' TODO: (maybe) find a way to pass 'non-production' status to scripts that
 #'       get sourced by R to satisfy dependencies... or perhaps non-production
 #'       should not be that convenient, perhaps it should be manually turned
 #'       on and off for each file and it's better not to inherit it.
-#' TODO: function to write out .trail as a JSON or XML file (to accompany 
+#' TODO: function to write out .trail as a JSON or XML file (to accompany
 #'       non-rdata saveouts)
 #' TODO: modify tread() to check for the existance of a trail flat-file as above
 #'       and read it if there is one. Then, treat it the same way
 #'       that a .trail from a loaded rdata file.
-#' TODO: modify tread() to see if the file being read is part of the repo and 
+#' TODO: modify tread() to see if the file being read is part of the repo and
 #'       document that fact, perhaps by the type being 'versioned-file' instead
 #'       of 'file'
-#' TODO: a twrite() function that is the inverse of tread() and creates an 
+#' TODO: a twrite() function that is the inverse of tread() and creates an
 #'       accompanying flat-file of trail.
-#' DONE: [priority] function to recursively print a .trail (to screen or to 
+#' DONE: [priority] function to recursively print a .trail (to screen or to
 #'       pander)
 #' DONE: wrap the repeating pattern of whichrecord, trail[whichrecord,], etc.
 #'       into a function.
@@ -40,35 +40,35 @@
 #' TODO: if tscript finds that the specified script is not part of the git repo
 #'       then record the type as 'unversioned-script' rather than 'script'
 #'       and save an md5 hash instead of a git-provided hash
-#' TODO: [priority] ~~possibly rename tscript() to tself() and~~ have a separate 
+#' TODO: [priority] ~~possibly rename tscript() to tself() and~~ have a separate
 #'       tsource() function specifically to wrap source
-#' TODO: [priority] check if a MYSTERY_FILE with a matching hash already exists 
+#' TODO: [priority] check if a MYSTERY_FILE with a matching hash already exists
 #'       and if it does, reuse the name
-#'       
-#' The below is an example of how a script can find out its own file-name 
+#'
+#' The below is an example of how a script can find out its own file-name
 #' (except if it is being interactively run)
 currentscript <- parent.frame(2)$ofile;
 if(is.null(currentscript)) currentscript <- 'RUN_FROM_INTERACTIVE_SESSION';
 
-#' 
-#' `fingerprint()` is supposed to be a more flexible and robust way to get 
+#'
+#' `fingerprint()` is supposed to be a more flexible and robust way to get
 #' reproducibility info about a file, and allows the same syntax to be used on
 #' any file whether it is tracked or not, and even if the file doesn't actually
 #' exist. In the future I plan to rewrite `gitstamp()`, `tload()`, and `tread()`
 #' to all use `fingerprint()` internally but as of right now they are independent
 #' from `fingerprint()`
-#' 
-#' It returns a list with elements that include... 
-#' 
+#'
+#' It returns a list with elements that include...
+#'
 #' * `in.repo`: whether or not the current working directory is a git repo
 #' * `repo`: URL of the git repo, NA if `in.repo` is false
 #' * `branch`: currently checked-out branch
-#' * `branch.on.origin`: whether the currently checked-out branch has already 
+#' * `branch.on.origin`: whether the currently checked-out branch has already
 #'   been pushed to origin... because that's not a given and we don't want this
 #'   scenario preventing reproduceability from taking us by surprise when we
 #'   least expect it
 #' * `last.branch.commit`: the hash of whatever is currently checked out
-#' * `uncommitted.changes`: whether or not there are uncommited changes to 
+#' * `uncommitted.changes`: whether or not there are uncommited changes to
 #'   any tracked files
 #' * `file.tracked`: if the optional `filename` argument is used, whether or not
 #'   that file is being tracked by git.
@@ -98,7 +98,7 @@ fingerprint <- function(filename=NA) {
     # what hash are we on?
     hsh <- system("git log --pretty=format:'%h' -n 1",intern=T);
     # what branch are we on? NOTE: don't know yet what happens in wierd cases,
-    # e.g. detached head, or stash or whatever... might return something 
+    # e.g. detached head, or stash or whatever... might return something
     # unexpected and error in a subsequent command, be aware
     brn <- system("git rev-parse --abbrev-ref HEAD",intern=T);
     # origin (i.e. what upstream repo does this project use for pushes/pulls)
@@ -108,9 +108,9 @@ fingerprint <- function(filename=NA) {
     # are there uncommitted changes?
     needupd <- suppressWarnings(
       system("git update-index --refresh && git diff-index HEAD --",intern = T));
-    
+
   }
-  # if for whatever reason git hash not available for a file, try to calculate 
+  # if for whatever reason git hash not available for a file, try to calculate
   # an md5sum(). If file doesn't exist (including NA) then this outputs 'NA'
   if(is.na(fhs)) fhs <- unname(tools::md5sum(c(na.omit(filename),'')[1]));
   return(list(
@@ -125,7 +125,7 @@ fingerprint <- function(filename=NA) {
 
 #' Return a commit hash (for inclusion in reports for example) after first making
 #' sure all changes are committed and pushed
-gitstamp <- function(production=T,branch=T) {
+gitstamp <- function(production=getOption('gitstamp_prod',TRUE),branch=T) {
   br<- if(branch) system("git rev-parse --abbrev-ref HEAD",intern=T) else NULL;
   if(production){
     if(length(gitdiff<-system("git update-index --refresh && git diff-index HEAD --",intern = T))!=0) stop(sprintf(
@@ -135,11 +135,11 @@ gitstamp <- function(production=T,branch=T) {
   } else return(c(br,'TEST_OUTPUT_DO_NOT_USE'));
 }
 
-# this creates a trail object if there isn't already one and either way 
+# this creates a trail object if there isn't already one and either way
 # returns a trail object
 tinit <- function(trail=getOption('trail'),...){
   if(is.null(trail)) {
-    trail<-data.frame(sequence=sprintf('%04d',1),time=Sys.time(),type='info',name='sessionInfo',value=NA,hash=NA,trail=NA,stringsAsFactors=F); 
+    trail<-data.frame(sequence=sprintf('%04d',1),time=Sys.time(),type='info',name='sessionInfo',value=NA,hash=NA,trail=NA,stringsAsFactors=F);
     trail$value <- list(sessionInfo());
     options(trail=trail);
   }
@@ -207,12 +207,12 @@ tseed <- function(seed,...){
   tupdate('seed',name=seedname,value=seed);
 }
 
-#' loading an rdata file and checking whether it has trail, to include in the 
+#' loading an rdata file and checking whether it has trail, to include in the
 #' the current trail as a nested data.frame
 tload <- function(file,envir=parent.frame()
                   ,verbose=FALSE,trailobj='.trail'){
   if(trailobj %in% ls(envir,all=T)) stop(sprintf('
-The object %s already exists, perhaps due to one of the trail-related functions 
+The object %s already exists, perhaps due to one of the trail-related functions
 crashing. Please try again in clean environment.',trailobj));
   filename <- deparse(match.call()$file);
   filehash <- tools::md5sum(file);
@@ -239,7 +239,7 @@ tread <- function(file,readfun,...){
 }
 
 #' Wrapper for save(). Pulls trail out of options as a data.frame, saves the
-#' data.frame along with whatever was originally going to be saved, and then 
+#' data.frame along with whatever was originally going to be saved, and then
 #' deletes it. Logs the save to itself before saving.
 tsave <- function(...,list=character(),envir=parent.frame(),trailobj='.trail'
                   ,verbose=TRUE){
